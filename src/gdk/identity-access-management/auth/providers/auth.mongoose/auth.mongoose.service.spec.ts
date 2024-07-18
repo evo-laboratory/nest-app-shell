8d47c7348f906825d3e95840ddeddf26b6868b8d
joelee <joe.evolab@gmail.com>
2024-07-18 22:32:26 +0800

    feat: setup provider services for auth

---
+++ src/gdk/identity-access-management/auth/providers/auth.mongoose/auth.mongoose.service.spec.ts
@@ -0,0 +1,18 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { AuthMongooseService } from './auth.mongoose.service';
+
+describe('AuthMongooseService', () => {
+  let service: AuthMongooseService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [AuthMongooseService],
+    }).compile();
+
+    service = module.get<AuthMongooseService>(AuthMongooseService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+});
