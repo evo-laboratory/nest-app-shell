c3e3cb1ad77f9a3c20c8384242b6057dbf4c022c
joelee <joe.evolab@gmail.com>
2024-07-19 11:53:54 +0800

    fix: import path

--- src/gdk/identity-access-management/user/types/user.interface.ts
+++ src/gdk/identity-access-management/user/types/user.interface.ts
@@ -1,4 +1,4 @@
-import { ROLE } from './types/role.enum';
+import { ROLE } from './role.enum';
 export interface IUser {
   authId: string; // * We do not expect populate Auth from User, please populate from Auth instead.
   email: string;
