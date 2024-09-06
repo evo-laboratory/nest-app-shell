2680a528178152f2066b2963488d749a0e005b62
joelee <joe.evolab@gmail.com>
2024-09-06 09:49:31 +0800

    feat: implement duplicated revoke check

--- src/gdk/identity-access-management/auth-revoked-token/providers/auth-revoked-token.mongoose/auth-revoked-token.mongoose.service.ts
+++ src/gdk/identity-access-management/auth-revoked-token/providers/auth-revoked-token.mongoose/auth-revoked-token.mongoose.service.ts
@@ -11,12 +11,17 @@ import { ClientSession, Model } from 'mongoose';
 
 import { MongoDBErrorHandler } from '@shared/mongodb/mongodb-error-handler';
 import { MethodLogger } from '@shared/winston-logger';
+import {
+  ERROR_CODE,
+  ERROR_SOURCE,
+  IUnitedHttpException,
+  UniteHttpException,
+} from '@shared/exceptions';
 
 import {
   AuthRevokedToken,
   AuthRevokedTokenDocument,
 } from './auth-revoked-token.schema';
-
 @Injectable()
 export class AuthRevokedTokenMongooseService
   implements AuthRevokedTokenService
@@ -34,7 +39,19 @@ export class AuthRevokedTokenMongooseService
     session?: ClientSession,
   ): Promise<IAuthRevokedToken> {
     try {
-      // TODO Check if already revoked => AUTH_REFRESH_TOKEN_ALREADY_REVOKED
+      const check = await this.AuthRevokedTokenModel.findOne({
+        authId: authId,
+        tokenId: tokenId,
+      });
+      if (check !== null) {
+        const error = this.buildError(
+          ERROR_CODE.AUTH_REFRESH_TOKEN_ALREADY_REVOKED,
+          `Already revoked`,
+          403,
+          'insert',
+        );
+        throw new UniteHttpException(error);
+      }
       const newData: AuthRevokedTokenDocument =
         await new this.AuthRevokedTokenModel({
           tokenId: tokenId,
@@ -87,5 +104,23 @@ export class AuthRevokedTokenMongooseService
     } catch (error) {
       return Promise.reject(MongoDBErrorHandler(error));
     }
+  }
+
+  @MethodLogger()
+  private buildError(
+    code: ERROR_CODE,
+    msg: string,
+    statusCode?: number,
+    methodName?: string,
+  ): IUnitedHttpException {
+    const errorObj: IUnitedHttpException = {
+      source: ERROR_SOURCE.NESTJS,
+      errorCode: code || ERROR_CODE.UNKNOWN,
+      message: msg,
+      statusCode: statusCode || 500,
+      contextName: 'AuthRevokedTokenMongooseService',
+      methodName: `${methodName}`,
+    };
+    return errorObj;
   }
 }
