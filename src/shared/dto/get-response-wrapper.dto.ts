f159ec69feb9d49c0f6ab958fdd33e10852d8c99
joelee <joe.evolab@gmail.com>
2024-09-30 21:58:28 +0800

    feat: add meta dto

--- src/shared/dto/get-response-wrapper.dto.ts
+++ src/shared/dto/get-response-wrapper.dto.ts
@@ -1,10 +1,12 @@
 import { AuthDto } from '@gdk-iam/auth/dto';
 import { ApiProperty } from '@nestjs/swagger';
 import { IGetResponseWrapper, IResponseMeta } from '@shared/types';
+import { ResponseMeta } from './response-meta.dto';
 
 export class GetResponseWrapper<T> implements IGetResponseWrapper<T> {
   @ApiProperty({ type: () => AuthDto })
   data: T;
+  @ApiProperty({ type: ResponseMeta })
   meta?: IResponseMeta;
   constructor(data: T, meta?: IResponseMeta) {
     this.data = data;
