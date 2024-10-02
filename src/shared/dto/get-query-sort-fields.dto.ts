223671fc9e3b101aaeca328cb58d9ec985fb7cbf
joelee <joe.evolab@gmail.com>
2024-10-03 07:28:16 +0800

    chore: remove not using

--- src/shared/dto/get-query-sort-fields.dto.ts
+++
@@ -1,9 +0,0 @@
-import { SORT } from '@shared/enums';
-import { IGetQuerySortFields } from '@shared/types';
-
-export class GetQuerySortFieldsDto implements IGetQuerySortFields {
-  [key: string]: SORT;
-  constructor(fields: { [key: string]: SORT }) {
-    Object.assign(this, fields); // Dynamically assign fields passed in the constructor
-  }
-}
--- src/shared/dto/index.ts
+++ src/shared/dto/index.ts
@@ -1,3 +1,2 @@
 export * from './get-query-options.dto';
-export * from './get-query-sort-fields.dto';
 export * from './response-meta.dto';
