8492943e5798743d057519844f996a223f5bc9a9
joelee <joe.evolab@gmail.com>
2024-10-03 07:26:52 +0800

    fix: cleanup

--- src/shared/dto/get-query-sort-fields.dto.ts
+++ src/shared/dto/get-query-sort-fields.dto.ts
@@ -4,11 +4,6 @@ import { IGetQuerySortFields } from '@shared/types';
 export class GetQuerySortFieldsDto implements IGetQuerySortFields {
   [key: string]: SORT;
   constructor(fields: { [key: string]: SORT }) {
-    Object.keys(fields).forEach((key) => {
-      if (fields[key] !== SORT.ASC && fields[key] !== SORT.DESC) {
-        throw new Error(`Invalid sort value for field ${key}: ${fields[key]}`);
-      }
-    });
     Object.assign(this, fields); // Dynamically assign fields passed in the constructor
   }
 }
