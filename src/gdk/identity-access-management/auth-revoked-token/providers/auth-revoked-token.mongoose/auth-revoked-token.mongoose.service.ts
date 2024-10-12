00a693c4ce961fc145fcf709650325edfe7996d4
Joe Lee <karocksjoelee@gmail.com>
2024-10-12 15:27:53 +0800

    feat: add verbose logger to revoked token

--- src/gdk/identity-access-management/auth-revoked-token/providers/auth-revoked-token.mongoose/auth-revoked-token.mongoose.service.ts
+++ src/gdk/identity-access-management/auth-revoked-token/providers/auth-revoked-token.mongoose/auth-revoked-token.mongoose.service.ts
@@ -2,7 +2,7 @@ import { AuthRevokedTokenService } from '@gdk-iam/auth-revoked-token/auth-revoke
 import { IAuthRevokedToken } from '@gdk-iam/auth-revoked-token/types';
 import { AUTH_REVOKED_TOKEN_MODEL_NAME } from '@gdk-iam/auth-revoked-token/statics';
 import { AUTH_TOKEN_TYPE } from '@gdk-iam/auth/types';
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { ClientSession, Model } from 'mongoose';
 import { MongoDBErrorHandler } from '@shared/mongodb';
@@ -24,10 +24,12 @@ import { AUTH_REVOKED_TOKEN_SOURCE } from '@gdk-iam/auth-revoked-token/enums';
 export class AuthRevokedTokenMongooseService
   implements AuthRevokedTokenService
 {
+  private readonly Logger = new Logger(AuthRevokedTokenMongooseService.name);
   constructor(
     @InjectModel(AUTH_REVOKED_TOKEN_MODEL_NAME)
     private readonly AuthRevokedTokenModel: Model<AuthRevokedToken>,
   ) {}
+
   @MethodLogger()
   public async insert(
     authId: string,
@@ -36,6 +38,11 @@ export class AuthRevokedTokenMongooseService
     type: AUTH_TOKEN_TYPE,
     session?: ClientSession,
   ): Promise<IAuthRevokedToken> {
+    this.Logger.verbose(authId, 'insert(authId)');
+    this.Logger.verbose(tokenId, 'insert(tokenId)');
+    this.Logger.verbose(source, 'insert(source)');
+    this.Logger.verbose(type, 'insert(type)');
+    this.Logger.verbose(session ? true : false, 'insert(session)');
     try {
       const check = await this.AuthRevokedTokenModel.findOne({
         authId: authId,
@@ -65,6 +72,8 @@ export class AuthRevokedTokenMongooseService
 
   @MethodLogger()
   public async check(authId: string, tokenId: string): Promise<boolean> {
+    this.Logger.verbose(authId, 'check(authId)');
+    this.Logger.verbose(tokenId, 'check(tokenId)');
     try {
       const found = await this.AuthRevokedTokenModel.findOne({
         authId: authId,
@@ -82,6 +91,8 @@ export class AuthRevokedTokenMongooseService
     tokenId: string,
   ): Promise<AuthRevokedTokenDocument> {
     try {
+      this.Logger.verbose(authId, 'get(authId)');
+      this.Logger.verbose(tokenId, 'get(tokenId)');
       const found = await this.AuthRevokedTokenModel.findOne({
         authId: authId,
         tokenId: tokenId,
@@ -94,6 +105,7 @@ export class AuthRevokedTokenMongooseService
 
   @MethodLogger()
   public async listByAuthId(authId: string): Promise<IAuthRevokedToken[]> {
+    this.Logger.verbose(authId, 'listByAuthId(authId)');
     try {
       const list = await this.AuthRevokedTokenModel.find({
         authId: authId,
