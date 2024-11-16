cb72128bf849efa27ddab2e42f960d80259e7625
Joe Lee <karocksjoelee@gmail.com>
2024-11-17 01:04:43 +0800

    test: EmailSignUp 99% coverage

--- test/gdk/auth-gpi.e2e.spec.ts
+++ test/gdk/auth-gpi.e2e.spec.ts
@@ -23,7 +23,7 @@ import {
 import { GPI, V1 } from '@shared/statics';
 import { AuthService } from '@gdk-iam/auth/auth.service';
 import { UserService } from '@gdk-iam/user/user.service';
-import { AUTH_API, EMAIL_SIGN_UP_PATH } from '@gdk-iam/auth/statics';
+import { AUTH_API, EMAIL_SIGN_UP_PATH, VERIFIED_EMAIL_SIGN_UP_PATH } from '@gdk-iam/auth/statics';
 import { IEmailSignUp } from '@gdk-iam/auth/types';
 import {
   AUTH_CODE_USAGE,
@@ -279,6 +279,79 @@ describe('GDK/AuthController', () => {
       expect(user.email).toBe(DTO.email);
       expect(user.firstName).toBe(DTO.firstName);
       expect(user.lastName).toBe(DTO.lastName);
+      expect(user.displayName).toBe(DTO.displayName);
+      expect(user.isEmailVerified).toBe(false);
+      expect(user.isSelfDeleted).toBe(false);
+      expect(user.backupAuth).toBe(null);
+      expect(user.selfDeletedAt).toBe(null);
+      expect(user.createdAt).toBeDefined();
+      expect(user.updatedAt).toBeDefined();
+      // * Validate the database state (AUTH)
+      const auth = await authService.getByEmail(DTO.email, {}, false);
+      expect(auth).toBeDefined();
+      expect(auth.data.identifier).toBe(DTO.email);
+      expect(auth.data.identifierType).toBe(AUTH_IDENTIFIER_TYPE.EMAIL);
+      if (process.env.DATABASE_PROVIDER === 'MONGODB') {
+        expect(auth.data.provider).toBe(AUTH_PROVIDER.MONGOOSE);
+      } else {
+        expect(auth.data.provider).toBe(
+          'FORCE THROW ERROR, PLEASE UPDATE THIS TEST CASE [AUTH STATE]',
+        );
+      }
+      expect(`${auth.data.userId}`).toEqual(`${user._id}`);
+      expect(auth.data.googleSignInId).toBeDefined();
+      expect(auth.data.appleSignInId).toBeDefined();
+      expect(auth.data.facebookSignId).toBeDefined();
+      expect(auth.data.githubSignId).toBeDefined();
+      expect(auth.data.gitlabSignId).toBeDefined();
+      expect(auth.data.microsoftSignId).toBeDefined();
+      expect(auth.data.password).toBeDefined();
+      expect(auth.data.code).toBeDefined();
+      expect(auth.data.codeUsage).toBe(AUTH_CODE_USAGE.SIGN_UP_VERIFY);
+      expect(auth.data.codeExpiredAt).toBeDefined();
+      expect(auth.data.isIdentifierVerified).toBe(false);
+      expect(auth.data.isActivated).toBe(true);
+      expect(auth.data.inactivatedAt).toBe(null);
+      expect(auth.data.createdAt).toBeDefined();
+      expect(auth.data.updatedAt).toBeDefined();
+      expect(auth.data.lastChangedPasswordAt).toBe(null);
+    });
+    it(`Valid EmailSignUpDto (empty displayName), should return 201 and validate database state`, async () => {
+      jest.spyOn(mailService, 'send').mockImplementationOnce(() => {
+        // * We are not testing the real mail service here, will test on MailController
+        return Promise.resolve({ mailId: 'mailId', statusText: '202' });
+      });
+      const DTO: IEmailSignUp = {
+        email: `jester_${new Date().getTime()}@user.com`,
+        password: `123456`,
+        firstName: 'fstName',
+        lastName: 'lstName',
+        displayName: '',
+      };
+      // * Validate the response
+      const res = await request(app.getHttpServer())
+        .post(`${EMAIL_SIGN_UP_GPI}`)
+        .set(ClientKeyHeader())
+        .send(DTO);
+      expect(res.status).toBe(201);
+      expect(res.body.email).toBe(DTO.email);
+      expect(res.body.isEmailSent).toBe(true);
+      expect(res.body.canResendAt).toBeDefined();
+      expect(res.body.provider).toBeDefined();
+      if (process.env.DATABASE_PROVIDER === 'MONGODB') {
+        expect(res.body.provider).toBe(AUTH_PROVIDER.MONGOOSE);
+      } else {
+        expect(res.body.provider).toBe(
+          'FORCE THROW ERROR, PLEASE UPDATE THIS TEST CASE [RES]',
+        );
+      }
+      // * Validate the database state (USER)
+      const user = await userService.findByEmail(DTO.email);
+      expect(user).toBeDefined();
+      expect(user.email).toBe(DTO.email);
+      expect(user.firstName).toBe(DTO.firstName);
+      expect(user.lastName).toBe(DTO.lastName);
+      expect(user.displayName).toBe(`${DTO.firstName} ${DTO.lastName}`);
       expect(user.isEmailVerified).toBe(false);
       expect(user.isSelfDeleted).toBe(false);
       expect(user.backupAuth).toBe(null);
@@ -316,6 +389,7 @@ describe('GDK/AuthController', () => {
       expect(auth.data.lastChangedPasswordAt).toBe(null);
     });
   });
+  const VERIFIED_EMAIL_SIGN_UP_GPI = `${TARGET_PATH}/${VERIFIED_EMAIL_SIGN_UP_PATH}`;
   // * --- End of TEST CASES ---
   afterAll(async () => {
     await app.close();
