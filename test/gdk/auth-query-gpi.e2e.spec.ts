b2d66ea78611cb0283f625e9a12200c1f72eb36e
Joe Lee <karocksjoelee@gmail.com>
2025-01-04 13:25:27 +0800

    test: add get test cases

--- test/gdk/auth-query-gpi.e2e.spec.ts
+++ test/gdk/auth-query-gpi.e2e.spec.ts
@@ -19,8 +19,10 @@ import {
 import { GPI, LIST_PATH, V1 } from '@shared/statics';
 import { AuthService } from '@gdk-iam/auth/auth.service';
 import { UserService } from '@gdk-iam/user/user.service';
-import { AUTH_API } from '@gdk-iam/auth/statics';
-import e from 'express';
+import { AUTH_API, EMAIL_PATH, IDENTIFIER_PATH } from '@gdk-iam/auth/statics';
+import { AUTH_IDENTIFIER_TYPE } from '@gdk-iam/auth/enums';
+import { IEmailSignUp } from '@gdk-iam/auth/types';
+import { MailService } from '@gdk-mail/mail.service';
 
 describe('GDK/AuthController', () => {
   const CONTROLLER_ENDPOINT = `/${GPI}/${AUTH_API}`;
@@ -36,6 +38,7 @@ describe('GDK/AuthController', () => {
   let app: INestApplication;
   let authService: AuthService;
   let userService: UserService;
+  let mailService: MailService;
   let sysOwnerAccessToken: string;
   let generalUserAccessToken: string;
   beforeAll(async () => {
@@ -53,6 +56,7 @@ describe('GDK/AuthController', () => {
     await app.init();
     userService = moduleFixture.get<UserService>(UserService);
     authService = moduleFixture.get<AuthService>(AuthService);
+    mailService = moduleFixture.get<MailService>(MailService);
     // * STEP 2. Create a system owner for Authorization
     const TestOwner = TestSysOwnerData(`${process.env.SYS_OWNER_EMAIL}`);
     const { accessToken } = await authService.emailSignIn({
@@ -141,6 +145,288 @@ describe('GDK/AuthController', () => {
       const fstData = res.body.data[0];
       negativeSelection.forEach((key) => {
         expect(fstData[key.replace('-', '')]).not.toBeDefined();
+      });
+    });
+  });
+  const GET_BY_EMAIL_API = `${TARGET_PATH}/${EMAIL_PATH}`;
+  describe(`[GET] ${GET_BY_EMAIL_API}/:email`, () => {
+    it(`ClientGuarded: ${process.env.CLIENT_KEY_NAME}, should return 403`, () => {
+      return request(app.getHttpServer())
+        .get(`${GET_BY_EMAIL_API}/${JESTER01_EMAIL}`)
+        .expect(403);
+    });
+    it(`Pass in ${process.env.CLIENT_KEY_NAME}, should return 401`, () => {
+      return request(app.getHttpServer())
+        .get(`${GET_BY_EMAIL_API}/${JESTER01_EMAIL}`)
+        .set(ClientKeyHeader())
+        .expect(401);
+    });
+    it(`EmptyBearerHeader, should return 401`, () => {
+      return request(app.getHttpServer())
+        .get(`${GET_BY_EMAIL_API}/${JESTER01_EMAIL}`)
+        .set(ClientKeyHeader())
+        .set(EmptyBearerHeader())
+        .expect(401);
+    });
+    it('BearerHeader (system-owner), should return 200', () => {
+      return request(app.getHttpServer())
+        .get(`${GET_BY_EMAIL_API}/${JESTER01_EMAIL}`)
+        .set(ClientKeyHeader())
+        .set(BearerHeader(sysOwnerAccessToken))
+        .send({})
+        .expect(200)
+        .expect((res) => {
+          expect(res.body.data).toBeDefined();
+          expect(res.body.data).toBeInstanceOf(Object);
+          expect(res.body.data.identifier).toBe(JESTER01_EMAIL);
+          expect(res.body.data.identifierType).toBe(AUTH_IDENTIFIER_TYPE.EMAIL);
+        });
+    });
+    it('Query with field selections (positive)', async () => {
+      // * Below should be one of the fields in the IAuth
+      const positiveSelect = [
+        'identifier',
+        'identifierType',
+        'userId',
+        'createdAt',
+      ];
+      const res = await request(app.getHttpServer())
+        .get(`${GET_BY_EMAIL_API}/${JESTER01_EMAIL}`)
+        .query({ fieldSelection: positiveSelect.join(',') })
+        .set(ClientKeyHeader())
+        .set(BearerHeader(sysOwnerAccessToken))
+        .send({});
+      expect(res.status).toBe(200);
+      expect(res.body.data).toBeDefined();
+      const testData = res.body.data;
+      Object.keys(testData).forEach((key) => {
+        expect(testData[key]).toBeDefined();
+      });
+      // * Expecting the length of the object to be the same as the positiveSelect, + 1 for the _id
+      expect(Object.keys(testData).length).toBe(positiveSelect.length + 1);
+    });
+    it('Query with field selections (negative)', async () => {
+      // * Below should be one of the fields in the IAuth
+      const negativeSelection = [
+        '-identifier',
+        '-identifierType',
+        '-userId',
+        '-createdAt',
+      ];
+      const res = await request(app.getHttpServer())
+        .get(`${GET_BY_EMAIL_API}/${JESTER01_EMAIL}`)
+        .query({ fieldSelection: negativeSelection.join(',') })
+        .set(ClientKeyHeader())
+        .set(BearerHeader(sysOwnerAccessToken))
+        .send({});
+      expect(res.status).toBe(200);
+      expect(res.body.data).toBeDefined();
+      const testData = res.body.data;
+      negativeSelection.forEach((key) => {
+        expect(testData[key.replace('-', '')]).not.toBeDefined();
+      });
+    });
+  });
+  const GET_BY_IDENTIFIER_API = `${TARGET_PATH}/${IDENTIFIER_PATH}`;
+  describe(`[GET] ${GET_BY_IDENTIFIER_API}/:email`, () => {
+    it(`ClientGuarded: ${process.env.CLIENT_KEY_NAME}, should return 403`, () => {
+      return request(app.getHttpServer())
+        .get(`${GET_BY_IDENTIFIER_API}/${JESTER01_EMAIL}`)
+        .expect(403);
+    });
+    it(`Pass in ${process.env.CLIENT_KEY_NAME}, should return 401`, () => {
+      return request(app.getHttpServer())
+        .get(`${GET_BY_IDENTIFIER_API}/${JESTER01_EMAIL}`)
+        .set(ClientKeyHeader())
+        .expect(401);
+    });
+    it(`EmptyBearerHeader, should return 401`, () => {
+      return request(app.getHttpServer())
+        .get(`${GET_BY_IDENTIFIER_API}/${JESTER01_EMAIL}`)
+        .set(ClientKeyHeader())
+        .set(EmptyBearerHeader())
+        .expect(401);
+    });
+    it('BearerHeader (system-owner), should return 200', () => {
+      return request(app.getHttpServer())
+        .get(`${GET_BY_IDENTIFIER_API}/${JESTER01_EMAIL}`)
+        .set(ClientKeyHeader())
+        .set(BearerHeader(sysOwnerAccessToken))
+        .send({})
+        .expect(200)
+        .expect((res) => {
+          expect(res.body.data).toBeDefined();
+          expect(res.body.data).toBeInstanceOf(Object);
+          expect(res.body.data.identifier).toBe(JESTER01_EMAIL);
+          expect(res.body.data.identifierType).toBe(AUTH_IDENTIFIER_TYPE.EMAIL);
+        });
+    });
+    it('Query with field selections (positive)', async () => {
+      // * Below should be one of the fields in the IAuth
+      const positiveSelect = [
+        'identifier',
+        'identifierType',
+        'userId',
+        'createdAt',
+      ];
+      const res = await request(app.getHttpServer())
+        .get(`${GET_BY_IDENTIFIER_API}/${JESTER01_EMAIL}`)
+        .query({ fieldSelection: positiveSelect.join(',') })
+        .set(ClientKeyHeader())
+        .set(BearerHeader(sysOwnerAccessToken))
+        .send({});
+      expect(res.status).toBe(200);
+      expect(res.body.data).toBeDefined();
+      const testData = res.body.data;
+      Object.keys(testData).forEach((key) => {
+        expect(testData[key]).toBeDefined();
+      });
+      // * Expecting the length of the object to be the same as the positiveSelect, + 1 for the _id
+      expect(Object.keys(testData).length).toBe(positiveSelect.length + 1);
+    });
+    it('Query with field selections (negative)', async () => {
+      // * Below should be one of the fields in the IAuth
+      const negativeSelection = [
+        '-identifier',
+        '-identifierType',
+        '-userId',
+        '-createdAt',
+      ];
+      const res = await request(app.getHttpServer())
+        .get(`${GET_BY_IDENTIFIER_API}/${JESTER01_EMAIL}`)
+        .query({ fieldSelection: negativeSelection.join(',') })
+        .set(ClientKeyHeader())
+        .set(BearerHeader(sysOwnerAccessToken))
+        .send({});
+      expect(res.status).toBe(200);
+      expect(res.body.data).toBeDefined();
+      const testData = res.body.data;
+      negativeSelection.forEach((key) => {
+        expect(testData[key.replace('-', '')]).not.toBeDefined();
+      });
+    });
+  });
+  const GET_BY_ID_API = `${TARGET_PATH}`;
+  describe(`[GET] ${GET_BY_ID_API}/:id`, () => {
+    it(`ClientGuarded: ${process.env.CLIENT_KEY_NAME}, should return 403`, () => {
+      return request(app.getHttpServer())
+        .get(`${GET_BY_ID_API}/${TEST_VALID_MONGODB_OBJECT_ID}`)
+        .expect(403);
+    });
+    it(`Pass in ${process.env.CLIENT_KEY_NAME}, should return 401`, () => {
+      return request(app.getHttpServer())
+        .get(`${GET_BY_ID_API}/${TEST_VALID_MONGODB_OBJECT_ID}`)
+        .set(ClientKeyHeader())
+        .expect(401);
+    });
+    it(`EmptyBearerHeader, should return 401`, () => {
+      return request(app.getHttpServer())
+        .get(`${GET_BY_ID_API}/${TEST_VALID_MONGODB_OBJECT_ID}`)
+        .set(ClientKeyHeader())
+        .set(EmptyBearerHeader())
+        .expect(401);
+    });
+    it(`Not exist id, should return 404`, () => {
+      return request(app.getHttpServer())
+        .get(`${GET_BY_ID_API}/${TEST_VALID_MONGODB_OBJECT_ID}`)
+        .set(ClientKeyHeader())
+        .set(BearerHeader(sysOwnerAccessToken))
+        .expect(404);
+    });
+    it(`Exist id, should return 200`, async () => {
+      // * Usually with MailService
+      jest.spyOn(mailService, 'send').mockImplementationOnce(() => {
+        // * We are not testing the real mail service here, will test on MailController
+        return Promise.resolve({ mailId: 'mailId', statusText: '202' });
+      });
+      // * Simulate sign up
+      const DTO: IEmailSignUp = {
+        email: `jester_${new Date().getTime()}@user.com`,
+        password: `123456`,
+        firstName: 'fstName',
+        lastName: 'lstName',
+        displayName: 'displayName',
+      };
+      await authService.emailSignUp(DTO, false);
+      const auth = await authService.getByEmail(DTO.email, {}, false);
+      return request(app.getHttpServer())
+        .get(`${GET_BY_ID_API}/${auth.data._id}`)
+        .set(ClientKeyHeader())
+        .set(BearerHeader(sysOwnerAccessToken))
+        .expect(200);
+    });
+    it('Query with field selections (positive)', async () => {
+      // * Usually with MailService
+      jest.spyOn(mailService, 'send').mockImplementationOnce(() => {
+        // * We are not testing the real mail service here, will test on MailController
+        return Promise.resolve({ mailId: 'mailId', statusText: '202' });
+      });
+      // * Simulate sign up
+      const DTO: IEmailSignUp = {
+        email: `jester_${new Date().getTime()}@user.com`,
+        password: `123456`,
+        firstName: 'fstName',
+        lastName: 'lstName',
+        displayName: 'displayName',
+      };
+      await authService.emailSignUp(DTO, false);
+      const auth = await authService.getByEmail(DTO.email, {}, false);
+      // * Below should be one of the fields in the IAuth
+      const positiveSelect = [
+        'identifier',
+        'identifierType',
+        'userId',
+        'createdAt',
+      ];
+      const res = await request(app.getHttpServer())
+        .get(`${GET_BY_ID_API}/${auth.data._id}`)
+        .query({ fieldSelection: positiveSelect.join(',') })
+        .set(ClientKeyHeader())
+        .set(BearerHeader(sysOwnerAccessToken))
+        .send({});
+      expect(res.status).toBe(200);
+      expect(res.body.data).toBeDefined();
+      const testData = res.body.data;
+      Object.keys(testData).forEach((key) => {
+        expect(testData[key]).toBeDefined();
+      });
+      // * Expecting the length of the object to be the same as the positiveSelect, + 1 for the _id
+      expect(Object.keys(testData).length).toBe(positiveSelect.length + 1);
+    });
+    it('Query with field selections (negative)', async () => {
+      // * Usually with MailService
+      jest.spyOn(mailService, 'send').mockImplementationOnce(() => {
+        // * We are not testing the real mail service here, will test on MailController
+        return Promise.resolve({ mailId: 'mailId', statusText: '202' });
+      });
+      // * Simulate sign up
+      const DTO: IEmailSignUp = {
+        email: `jester_${new Date().getTime()}@user.com`,
+        password: `123456`,
+        firstName: 'fstName',
+        lastName: 'lstName',
+        displayName: 'displayName',
+      };
+      await authService.emailSignUp(DTO, false);
+      const auth = await authService.getByEmail(DTO.email, {}, false);
+      // * Below should be one of the fields in the IAuth
+      const negativeSelection = [
+        '-identifier',
+        '-identifierType',
+        '-userId',
+        '-createdAt',
+      ];
+      const res = await request(app.getHttpServer())
+        .get(`${GET_BY_ID_API}/${auth.data._id}`)
+        .query({ fieldSelection: negativeSelection.join(',') })
+        .set(ClientKeyHeader())
+        .set(BearerHeader(sysOwnerAccessToken))
+        .send({});
+      expect(res.status).toBe(200);
+      expect(res.body.data).toBeDefined();
+      const testData = res.body.data;
+      negativeSelection.forEach((key) => {
+        expect(testData[key.replace('-', '')]).not.toBeDefined();
       });
     });
   });
